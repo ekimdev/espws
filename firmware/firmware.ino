dfc4c86633f5651226c6eaa55725c49b0539a480
Rodrigo Acosta <rodrigoacosta444@gmail.com>
2022-04-08 14:25:50 -0300

    first commit: add espws proyect structure

---
+++ firmware/firmware.ino
@@ -0,0 +1,47 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+#define BROKER_URL "192.168.0.68"
+#define MSG_SIZE 24
+
+const char* ssid = "Fibertel WiFi400 2.4GHz";
+const char* password = "24RoBrii1910";
+char msg[MSG_SIZE];
+
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient wifi_client;
+PubSubClient mqtt_client(wifi_client);
+
+void setup(void)
+{
+  Serial.begin(115200);
+
+  dht.begin();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.print("Connected");
+
+  mqtt_client.setServer(BROKER_URL, 1883);
+  delay(2000);
+  mqtt_client.connect("ESP8266_1");
+}
+
+
+void loop()
+{
+ mqtt_client.loop();
+
+ float t = dht.readTemperature();
+ snprintf(msg, MSG_SIZE, "%f", t);
+ mqtt_client.publish("hola/mundo", msg);
+ delay(2000);
+}
